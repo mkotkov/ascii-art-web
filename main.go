56d1741c273c6faee7c7d9e7f43eb0c3200817c0
mkotkov <maximkotkov@gmail.com>
2023-10-02 13:34:57 +0300

    finale

--- main.go
+++ main.go
@@ -1,82 +1,78 @@
 package main
 
 import (
-    "ascii-art-web/static"
-    "html/template"
-    "net/http"
-    "bytes"
+	"bytes"
+	"html/template"
+	"net/http"
+
+	"ascii-art-web/static"
 )
 
 type banner struct {
-    ASCIIArt string
+	ASCIIArt string
 }
 
 type responseWriterWithBuffer struct {
-    http.ResponseWriter
-    buf *bytes.Buffer
+	http.ResponseWriter
+	buf *bytes.Buffer
 }
 
 func (rw *responseWriterWithBuffer) Write(p []byte) (int, error) {
-    return rw.buf.Write(p)
+	return rw.buf.Write(p)
 }
 
 func asciiart(input, fonts string) string {
-    fontFolder := "static/fonts"
-    var buf bytes.Buffer
-    rw := &responseWriterWithBuffer{buf: &buf}
-    ascii_art.AsciiArt(input, fonts, rw, fontFolder)
-    return buf.String()
+	fontFolder := "static/fonts"
+	var buf bytes.Buffer
+	rw := &responseWriterWithBuffer{buf: &buf}
+	ascii_art.AsciiArt(input, fonts, rw, fontFolder)
+	return buf.String()
 }
 
 func handleRequest() {
-    http.HandleFunc("/", mainPage)
-    http.HandleFunc("/ascii-art", asciiArtHandler)
-    http.ListenAndServe(":8080", nil)
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
+
+	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/ascii-art", asciiArtHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
-    tmpl, err := template.ParseFiles("templates/index.html", "templates/styles/styles.css")
+	tmpl, err := template.ParseFiles("templates/index.html", "templates/styles/styles.css")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
-    tmpl.Execute(w, "")
+	tmpl.Execute(w, "")
 }
 
 func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("templates/index.html", "templates/styles/styles.css")
+	tmpl, err := template.ParseFiles("templates/index.html", "templates/styles/styles.css")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-      
-    r.ParseForm()
-    input := r.FormValue("input")
-    fonts := r.FormValue("fonts")
+	r.ParseForm()
+	input := r.FormValue("input")
+	fonts := r.FormValue("fonts")
 
-    if input == "" || fonts == "" {
-        http.Error(w, "Missing input data", http.StatusBadRequest)
-        return
-    }
+	if input == "" || fonts == "" {
+		http.Error(w, "Missing input data", http.StatusBadRequest)
+		return
+	}
 
-    asciiArtResult := asciiart(input, fonts)
+	asciiArtResult := asciiart(input, fonts)
 
-    pageData := banner{
-        ASCIIArt: asciiArtResult,
-    }
+	pageData := banner{
+		ASCIIArt: asciiArtResult,
+	}
 
-    tmpl.Execute(w, pageData)
+	tmpl.Execute(w, pageData)
 }
 
 func main() {
-    handleRequest()
+	handleRequest()
 }
-
-
--- static/ascii_art.go
+++ static/ascii_art.go
@@ -1,61 +1,61 @@
 package ascii_art
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "net/http"
-    "path/filepath"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func AsciiArt(input string, fonts string, page http.ResponseWriter, fontFolder string) {
-    fontFilePath := filepath.Join(fontFolder, fonts+".txt")
-    var data []byte
+	fontFilePath := filepath.Join(fontFolder, fonts+".txt")
+	var data []byte
 
-    data, err := os.ReadFile(fontFilePath)
-    if err != nil {
-        http.Error(page, fmt.Sprintf("Error reading font file: %v", err), http.StatusInternalServerError)
-        return
-    }
+	data, err := os.ReadFile(fontFilePath)
+	if err != nil {
+		http.Error(page, fmt.Sprintf("Error reading font file: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-    input = strings.Replace(input, "\\n", "\n" , -1)// change "\\n" to "\n"
-    inputText := strings.Split(input, getNewlineSeparator([]byte(input)))
-    fontString := strings.Split(string(data), getNewlineSeparator(data))
+	input = strings.Replace(input, "\\n", "\n", -1) // change "\\n" to "\n"
+	inputText := strings.Split(input, getNewlineSeparator([]byte(input)))
+	fontString := strings.Split(string(data), getNewlineSeparator(data))
 
-    for _, text := range inputText {
-        if text == "" {
-            fmt.Fprintln(page) // Вывести пустую строку
-        } else {
-            for i := 0; i < 9; i++ {
-                for _, char := range text {
-                    start, _ := getLetter(char)
-                    fmt.Fprint(page, fontString[start+i])
-                }
-                fmt.Fprintln(page) // Перейти на новую строку после строки букв
-            }
-        }
-    }
+	for _, text := range inputText {
+		if text == "" {
+			fmt.Fprintln(page) // Вывести пустую строку
+		} else {
+			for i := 0; i < 9; i++ {
+				for _, char := range text {
+					start, _ := getLetter(char)
+					fmt.Fprint(page, fontString[start+i])
+				}
+				fmt.Fprintln(page) // Перейти на новую строку после строки букв
+			}
+		}
+	}
 }
 
 func getLetter(c rune) (int, int) {
-    charList := " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz{|}~"
-    index := strings.IndexRune(charList, c)
-    if index == -1 {
-        fmt.Println("Missing input characters")
-        return -1, -1
-    }
-    firstLine := index * 9
-    lastLine := firstLine + 8
-    return firstLine, lastLine
+	charList := " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz{|}~"
+	index := strings.IndexRune(charList, c)
+	if index == -1 {
+		fmt.Println("Missing input characters")
+		return -1, -1
+	}
+	firstLine := index * 9
+	lastLine := firstLine + 8
+	return firstLine, lastLine
 }
 
 func error(page http.ResponseWriter) {
-    http.Error(page, "Invalid input.", http.StatusBadRequest)
+	http.Error(page, "Invalid input.", http.StatusBadRequest)
 }
 
 func getNewlineSeparator(data []byte) string {
-    if strings.ContainsRune(string(data), '\r') {
-        return "\r\n" // Windows-style
-    }
-    return "\n" // Unix-style
+	if strings.ContainsRune(string(data), '\r') {
+		return "\r\n" // Windows-style
+	}
+	return "\n" // Unix-style
 }
